59ccaa5bbc9e56dd97a78f6329eb200b244b0b8c
bodya17 <levantovychbogdan@gmail.com>
2016-11-07 23:04:19 +0200

    add interactivity

--- main.js
+++ main.js
@@ -1,37 +1,34 @@
 var b = JXG.JSXGraph.initBoard('box', {axis: true, boundingbox: [-1, 3000, 50, -1]});
 
-xaxis = b.create('axis', [[0, 0], [1,0]], { name:'час',
-			withLabel: true,
-			label: { position: 'rt',  // possible values are 'lft', 'rt', 'top', 'bot'
-					 offset: [-15, 20]   // (in pixels)
-					 }
-			});
-yaxis = b.create('axis', [[0, 0], [0, 1]], { name:'FV',
-			withLabel: true,
-			label: {
-			  position: 'rt',  // possible values are 'lft', 'rt', 'top', 'bot'
-			  offset: [-20, 0]   // (in pixels)
-				}
-			});
-b.create('functiongraph', [ x => 1000 * Math.pow(1.03, x), 0], {strokeColor:'#0effe0', strokeWidth: 3});
-// b.create('functiongraph', [ x => 1000 * Math.pow(1.1, x), 0], {strokeColor:'#0effe0', strokeWidth: 3});
-// b.create('functiongraph', [ x => 1 / (0.47 - 0.1599 * Math.log(x)), 0], {strokeColor:'#ff00a0', strokeWidth: 3});
+b.create('axis', [[0, 0], [1,0]], { name:'час', withLabel: true, label: {
+    position: 'rt',
+    offset: [-15, 20]
+  }
+});
+
+b.create('axis', [[0, 0], [0, 1]], { name:'FV', withLabel: true, label: {
+    position: 'rt',
+    offset: [-20, 0]
+  }
+});
+
 
 var app = angular.module('calc', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+
 app.constant("moment", moment);
 
 app.controller('calculatorCtrl', function ($scope, $filter) {
 
   $scope.options = {
-    // customClass: getDayClass,
-    minDate: new Date(),
     showWeeks: false
   };
 
   $scope.pv = 1000;
-  $scope.years = 1;
-  $scope.interest = 0.1;
-  $scope.type = 'Складні';
+	$scope.fv = 2000;
+  $scope.n = 1;
+  $scope.d = 0.1;
+
+  $scope.graph;
 
   $scope.difference = function () {
     var a = moment($scope.start);
@@ -39,45 +36,15 @@ app.controller('calculatorCtrl', function ($scope, $filter) {
     return b.diff(a, 'days');
   }
 
-
-  $scope.future = function () {
-    if ($scope.type == "Прості") {
-      return $scope.pv * (1 + $scope.interest * $scope.years);
-    }
-    return $scope.pv * Math.pow(1 + $scope.interest, $scope.years);
-  };
-
-  $scope.assetsStart   = 25000000;
-  $scope.assestsEnd    = 28000000;
-  $scope.investIncome  = 2125000;
-  $scope.saleIncome    = 1500000;
-  $scope.restOutgoings = 750000;
-
-  $scope.d1 = 0.08;
-  $scope.d2 = 0.12;
-
-  // $scope.money;
-
-  // $scope.$watch('assetsStart', function(value) {
-  //     $scope.money = $filter('currency')($scope.assetsStart);
-  // });
-
-  $scope.salary = function () {
-    let sal = $scope.saleIncome + $scope.investIncome - $scope.restOutgoings -
-      $scope.assestsEnd * Math.pow(1 - $scope.d2 / 2, 2) + $scope.assetsStart / Math.pow(1 - $scope.d1 / 2, 2);
-    return sal.toFixed(2);
-  }
-
-// 5 260 936,11 ₴
-
-  // $scope.salary = $scope.saleIncome + $scope.investIncome - $scope.restOutgoings -
-  //           $scope.assestsEnd * Math.pow(1 - $scope.d2 / 2, 2) + $scope.assetsStart / Math.pow(1 - $scope.d1 / 2, 2);
-  //
-  // console.log(salary);
-  // // check
-  //
-  // let check = ($scope.assetsStart / (Math.pow(1 - $scope.d1 / 2, 2)) + $scope.saleIncome + $scope.investIncome
-  //               - $scope.salary - $scope.restOutgoings) / (Math.pow(1 - d$scope.2 / 2, 2));
-  // console.log(check);
-
+	$scope.evaluate = function () {
+    console.log('evaluate');
+		switch ($('input[name=unknown]:checked').val()) {
+			case 'pv': $scope.pv = $scope.fv * Math.pow(1 - $scope.d, $scope.n); break;
+			case 'fv': $scope.fv = $scope.pv / Math.pow(1 - $scope.d, $scope.n); break;
+			case 'n': $scope.n = Math.log($scope.pv / $scope.fv) / Math.log(1 - $scope.d); break;
+			case 'd': $scope.d = Math.pow($scope.pv / $scope.fv, 1 / $scope.n) - 1; break;
+		}
+    b.removeObject($scope.graph);
+		$scope.graph = b.create('functiongraph', [ x => $scope.pv / Math.pow(1 - $scope.d, x), 0], {strokeColor:'#0effe0', strokeWidth: 3});
+	}
 });
